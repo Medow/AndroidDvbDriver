7f16e13445a8eb87eda57b9ad2411d1ee0797775
Medow <baharmahir@yahoo.com>
2025-06-20 15:45:56 +0200

    Update MygicaPT362.java

--- drivers/src/main/java/info/martinmarinov/drivers/usb/cxusb/MygicaPT362.java
+++ drivers/src/main/java/info/martinmarinov/drivers/usb/cxusb/MygicaPT362.java
@@ -1,23 +1,3 @@
-/*
- * This is an Android user space port of DVB-T Linux kernel modules.
- *
- * Copyright (C) 2022 by Signalware Ltd <driver at aerialtv.eu>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
 package info.martinmarinov.drivers.usb.cxusb;
 
 import android.content.Context;
@@ -78,7 +58,7 @@ class MygicaPT362 extends CxUsbDvbDevice {
     @Override
     protected DvbTuner tunerAttatch() throws DvbException {
         // PT362 Type-C uses Si2157 tuner
-        return new Si2157(resources, i2CAdapter, frontend.gateControl(), 0x60, true, SI2157_CHIPTYPE_SI2157);
+        return new Si2157(resources, i2CAdapter, frontend.gateControl(), 0x60, false, SI2157_CHIPTYPE_SI2141);
     }
 
     @Override
