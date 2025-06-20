d964dae6e1a5c8056ed7c07cce1a8fd13a4b33b5
Medow <baharmahir@yahoo.com>
2025-06-20 13:54:24 +0200

    Update MygicaPT362.java

--- drivers/src/main/java/info/martinmarinov/drivers/usb/cxusb/MygicaPT362.java
+++ drivers/src/main/java/info/martinmarinov/drivers/usb/cxusb/MygicaPT362.java
@@ -1,1 +1,32 @@
 
+package info.martinmarinov.drivers.usb.cxusb;
+
+import android.content.Context;
+import android.hardware.usb.UsbDevice;
+
+import info.martinmarinov.drivers.DeviceFilter;
+import info.martinmarinov.drivers.DvbException;
+import info.martinmarinov.drivers.usb.DvbUsbIds;
+
+public class MygicaPT362 extends CxUsbDvbDevice {
+
+    final static DeviceFilter MYGICA_PT362 = new DeviceFilter(DvbUsbIds.USB_VID_CONEXANT,
+            DvbUsbIds.USB_PID_MYGICA_PT362, "Mygica PT362 DVB-T/T2 (Type-C)");
+
+    public MygicaPT362(UsbDevice usbDevice, Context context) throws DvbException {
+        super(usbDevice, context);
+    }
+
+    @Override
+    protected void configureDevice() throws DvbException {
+        // Si2168 demodulator + Si2157 tuner configuration
+        // Type-C specific power control
+        // DVB-T/T2 support
+        // This section would contain the actual low-level configuration for the device.
+        // For now, we'll leave it as a placeholder as the exact implementation details
+        // for the Si2168 and Si2157 are not provided in the summary.
+        // This would typically involve writing to specific registers on the device.
+    }
+}
+
+
